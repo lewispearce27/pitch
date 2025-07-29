bd0d66d3f29cad5c76078d05865b9456b78f49c2
lewispearce27 <lewis.pearce@gmail.com>
2025-07-29 17:53:31 +0100

    Create ppcustom-admin.js

---
+++ admin/js/ppcustom-admin.js
@@ -0,0 +1,32 @@
+jQuery(function($) {
+    $('#_ppcustom_category_id').on('change', function() {
+        var categoryId = $(this).val();
+        var designSelect = $('#_ppcustom_design_id');
+        designSelect.html('<option value="">Loading...</option>');
+
+        if (!categoryId) {
+            designSelect.html('<option value="">Select a design…</option>');
+            return;
+        }
+
+        $.post(ppcustom_admin.ajaxUrl, {
+            action: 'ppcustom_fetch_designs',
+            categoryId: categoryId
+        }, function(response) {
+            if (response.data) {
+                designSelect.html('<option value="">Select a design…</option>');
+                response.data.forEach(function(design) {
+                    var selected = (design.id === ppcustom_admin.selectedDesign) ? 'selected' : '';
+                    designSelect.append('<option value="' + design.id + '" ' + selected + '>' + design.title + '</option>');
+                });
+            } else {
+                designSelect.html('<option value="">No designs found</option>');
+            }
+        });
+    });
+
+    // Trigger on page load if a category was previously saved
+    if ($('#_ppcustom_category_id').val()) {
+        $('#_ppcustom_category_id').trigger('change');
+    }
+});
