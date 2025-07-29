7bfd71ceacf14367e779f11923fe621b6e303191
lewispearce27 <lewis.pearce@gmail.com>
2025-07-29 15:56:20 +0100

    Update product-meta.php

--- admin/product-meta.php
+++ admin/product-meta.php
@@ -1,36 +1,55 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
+/**
+ * Add PitchPrint fields to WooCommerce product admin
+ */
 add_action('woocommerce_product_options_general_product_data', function() {
+
+    error_log('PitchPrint product meta loaded'); // debug log
+
     echo '<div class="options_group">';
-    // Design ID
+
+    // Template ID field
     woocommerce_wp_text_input([
-        'id' => '_ppcustom_design_id',
-        'label' => 'PitchPrint Design ID',
-        'desc_tip' => true,
-        'description' => 'Enter the PitchPrint template ID for this product.'
+        'id'          => '_ppcustom_design_id',
+        'label'       => __('PitchPrint Design ID', 'ppcustom'),
+        'desc_tip'    => true,
+        'description' => __('Enter the PitchPrint template ID for this product.', 'ppcustom'),
     ]);
 
-    // Button Display Mode
+    // Dropdown for buttons
     woocommerce_wp_select([
-        'id'      => '_ppcustom_button_mode',
-        'label'   => 'PitchPrint Buttons',
-        'options' => [
+        'id'          => '_ppcustom_button_mode',
+        'label'       => __('PitchPrint Buttons', 'ppcustom'),
+        'options'     => [
             'both'    => 'Show Both Buttons',
             'design'  => 'Design Online Only',
             'upload'  => 'Upload Artwork Only',
         ],
-        'desc_tip' => true,
-        'description' => 'Select which buttons to show on the product page.'
+        'desc_tip'    => true,
+        'description' => __('Choose which PitchPrint buttons to show on the product page.', 'ppcustom'),
     ]);
+
     echo '</div>';
 });
 
+/**
+ * Save PitchPrint fields
+ */
 add_action('woocommerce_process_product_meta', function($post_id) {
     if (isset($_POST['_ppcustom_design_id'])) {
-        update_post_meta($post_id, '_ppcustom_design_id', sanitize_text_field($_POST['_ppcustom_design_id']));
+        update_post_meta(
+            $post_id,
+            '_ppcustom_design_id',
+            sanitize_text_field($_POST['_ppcustom_design_id'])
+        );
     }
     if (isset($_POST['_ppcustom_button_mode'])) {
-        update_post_meta($post_id, '_ppcustom_button_mode', sanitize_text_field($_POST['_ppcustom_button_mode']));
+        update_post_meta(
+            $post_id,
+            '_ppcustom_button_mode',
+            sanitize_text_field($_POST['_ppcustom_button_mode'])
+        );
     }
 });
